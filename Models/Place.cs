7a56181ee4240abeb1f59e681ca02c2817af38a8
Natalie Murphy <Nataliemurphy500@gmail.com>
2020-10-26 11:30:16 -0700

    Models: updates Place.cs with validations

--- Models/Place.cs
+++ Models/Place.cs
@@ -1,14 +1,20 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 namespace TravelApi.Models
 {
   public class Place
   {
+    [Required]
     public int PlaceId { get; set; }
+    [Required]
     public string Name { get; set; }
+    [Required]
 
     public string Review { get; set; }
+    [Required]
     public DateTime Date { get; set; }
-
+    [Required]
+    [Range(1, 10, ErrorMessage = "rating must be between 1 and 10.")]
     public int Rating { get; set; }
   }
 }
